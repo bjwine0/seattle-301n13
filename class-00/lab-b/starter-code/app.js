342bc818d26965e09ebcb4b873dedae606426ced
bjwine0 <bradsmilek@gmail.com>
2019-04-07 11:38:52 -0700

    modified app.js 1-9

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -108,7 +108,7 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-const sum = (a, b, c, d) => `${a + b + c + d}`;
+const sum = (a, b, c, d) => a + b + c + d;
 
 
 // TODO: Uncomment the following line of code to see the output in the browser console
@@ -135,12 +135,11 @@ const sumAndProduct = (a, b) => {
 console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
+const message = name => `Hello, ${name}!`;
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+console.log(message('Allie'));
 
 
 let Student = function(name, age, hometown) {
@@ -153,7 +152,7 @@ let joe = new Student('Joe', 'Schmoe', 100);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 
 Student.prototype.greeting = function() {
@@ -162,15 +161,14 @@ Student.prototype.greeting = function() {
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+Student.courseName = () => 'This student is enrolled in Code 301.';
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
